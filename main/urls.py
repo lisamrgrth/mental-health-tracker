98c62f9e9b6128c2668784e8022668f276c1f39f
lisamrgrth <lisatobing1610@gmail.com>
2024-11-13 14:37:28 +0700

    SELESAI

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,4 @@
-from main.views import delete_mood, add_mood_entry_ajax
+from main.views import create_mood_flutter, delete_mood, add_mood_entry_ajax
 from main.views import edit_mood
 from main.views import logout_user
 from main.views import login_user
@@ -21,4 +21,5 @@ urlpatterns = [
     path('edit-mood/<uuid:id>', edit_mood, name='edit_mood'),
     path('delete/<uuid:id>', delete_mood, name='delete_mood'), 
     path('create-mood-entry-ajax', add_mood_entry_ajax, name='add_mood_entry_ajax'),
+    path('create-flutter/', create_mood_flutter, name='create_mood_flutter'),
 ]
--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -28,6 +28,12 @@ DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "lisa-margaretha-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
 
 # Application definition
 
@@ -38,7 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+    'authentication',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'mental_health_tracker.urls'
